43816e96242add8441a803d0549e48c28f6cb309
Courtney Larson <larso126@msu.edu>
2024-06-21 09:07:50 -0500

    Setting up r code

---
+++ HuRon2022CSMIZooplankton.R
@@ -0,0 +1,15 @@
+#R code for Lake Huron 2022 CSMI survey report EPA ORD
+#Courtney Larson and Noah Grode
+
+#libraries
+library(reshape2)
+#programs
+
+#color vectors
+
+#Upload files
+Zoopreshap <- read.csv("ZoopforR6.21.24.csv", header=T)
+
+#melt to long format
+Zooplong<-melt(Zoopreshap, value.name="Count", 
+               variable.name = "Species")
