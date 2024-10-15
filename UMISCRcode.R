b9444d02bad46ab6d7b2bf5f9bae78c103b892e0
Noah Grode <Grode.Noah@epa.gov>
2024-10-15 15:00:33 -0500

    Made line graphs and box and whisker graphs

--- UMISCRcode.R
+++ UMISCRcode.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(tidyverse)
 library(reshape2)
 library(lubridate)
+library(readr)
 
 #Upload datasets
 
@@ -79,9 +80,90 @@ cors<-cor(HuronCSMIWide[,7:ncol(HuronCSMIWide)])
 #Visualizations
 #Make dataframe for faceted dreissena and swf density graph
 
-#Time-series line graphs
-BythoPrey<-subset(Zoopcount, Species == c("Leptodiaptomusminutus", "Leptodiaptomussicilis", "Daphniaspp", "Daphniaparvula", "Daphniaretrocurva", "Leptodiaptomusashlandi", "Daphnialongiremis", "Leptodorakindti", "Daphniagaleatamendotae", "Diaphanosomaspp", "Bythotrepheslongimanus"), select = SiteID:Density)
-BythoPrey2<-Zoopcount %>% filter(Species == c("Leptodiaptomusminutus", "Leptodiaptomussicilis", "Daphniaspp", "Daphniaparvula", "Daphniaretrocurva", "Leptodiaptomusashlandi", "Daphnialongiremis", "Leptodorakindti", "Daphniagaleatamendotae", "Diaphanosomaspp", "Bythotrepheslongimanus"))
-nrow(Zoopcount)
-table(Zoopcount$Species)
-unique(Zoopcount$Species)
+#######Time-series line graphs
+#Uploading Genus key to cut down on how many panels - binned species into genus
+GenusKey <- read_csv("GenusKey.csv")
+
+#subset data
+BythoPrey2<-Zoopcount %>% filter(Species %in% c("Leptodiaptomusminutus", "Leptodiaptomussicilis", "Daphniaspp", "Daphniaparvula", "Daphniaretrocurva", "Leptodiaptomusashlandi", "Daphnialongiremis", "Leptodorakindti", "Daphniagaleatamendotae", "Diaphanosomaspp", "Bythotrepheslongimanus"))
+#aggregate
+BythoPreyagg<-aggregate(`Density` ~ Species + Area + Month, data = BythoPrey2, FUN = mean)
+
+#Merge bythopreyagg and genus key
+BythoPrey22<-merge(BythoPrey2,GenusKey, by="Species")
+
+#aggregate again
+BythoPreyMEAN<-aggregate(`Density` ~ Genus + Area + Month, data = BythoPrey22, FUN = mean)
+BythoPreySD<-aggregate(`Density` ~ Genus + Area + Month, data = BythoPrey22, FUN = sd)
+BythoPreyMEANSD2<-BythoPrey22 %>% group_by(Genus,Area,Month) %>% summarise(mean = mean(Density), std = sd(Density))
+
+
+#factor
+
+#Subset genus and area so graph separetly
+Bytho<-BythoPreyMEANSD2 %>% filter(Genus %in% "Bythotrephes longimanus")
+Daphnia<-BythoPreyMEANSD2 %>% filter(Genus %in% "Daphnia")
+Diaphanosoma<-BythoPreyMEANSD2 %>% filter(Genus %in% "Diaphanosoma")
+Leptodiaptomus<-BythoPreyMEANSD2 %>% filter(Genus %in% "Leptodiaptomus")
+Leptodora<-BythoPreyMEANSD2 %>% filter(Genus %in% "Leptodora")
+
+#factor regions
+Bytho$Area<-factor(Bytho$Area, c("NC", "SB", "GB", "SMB", "NMB"))
+Bytho$Month<-factor(Bytho$Month, c("June", "July", "Aug"))
+Daphnia$Area<-factor(Daphnia$Area, c("NC", "SB", "GB", "SMB", "NMB"))
+Daphnia$Month<-factor(Daphnia$Month, c("June", "July", "Aug"))
+Diaphanosoma$Area<-factor(Diaphanosoma$Area, c("NC", "SB", "GB", "SMB", "NMB"))
+Diaphanosoma$Month<-factor(Diaphanosoma$Month, c("June", "July", "Aug"))
+Leptodiaptomus$Area<-factor(Leptodiaptomus$Area, c("NC", "SB", "GB", "SMB", "NMB"))
+Leptodiaptomus$Month<-factor(Leptodiaptomus$Month, c("June", "July", "Aug"))
+Leptodora$Area<-factor(Leptodora$Area, c("NC", "SB", "GB", "SMB", "NMB"))
+Leptodora$Month<-factor(Leptodora$Month, c("June", "July", "Aug"))
+
+#Box and Whisker graph, bytho and prey
+
+ggplot(Bytho, aes(x=Month, y=mean, group=Month))+
+  geom_boxplot()+geom_boxplot()+
+  scale_fill_manual(values=c("lightgreen","springgreen3","darkgreen"))+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"),
+        axis.title.x=element_text(size=10),axis.title.y=element_text(size=10),
+        axis.text.x=element_text(size=10),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=14),legend.text = element_text(size=14))+
+    facet_grid(.~Area)
+
+ggplot(Daphnia, aes(x=Month, y=mean, group=Area)) +
+  geom_line(aes(color=Area))+
+  geom_point(aes(color=Area))+
+  geom_errorbar(aes(ymin=mean-std, ymax=mean+std, color=Area))+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"),
+        axis.title.x=element_text(size=10),axis.title.y=element_text(size=10),
+        axis.text.x=element_text(size=10),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=14),legend.text = element_text(size=14))+
+  facet_grid(.~Area, scales = "free")
+
+ggplot(Diaphanosoma, aes(x=Month, y=mean, group=Area)) +
+  geom_line(aes(color=Area))+
+  geom_point(aes(color=Area))+
+  geom_errorbar(aes(ymin=mean-std, ymax=mean+std, color=Area))+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"),
+        axis.title.x=element_text(size=10),axis.title.y=element_text(size=10),
+        axis.text.x=element_text(size=10),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=14),legend.text = element_text(size=14))+
+  facet_grid(.~Area, scales = "free")
+
+ggplot(Leptodiaptomus, aes(x=Month, y=Density, group=Area)) +
+  geom_line(aes(color=Area))+
+  geom_point(aes(color=Area))+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"),
+        axis.title.x=element_text(size=10),axis.title.y=element_text(size=10),
+        axis.text.x=element_text(size=10),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=14),legend.text = element_text(size=14))+
+  facet_grid(.~Area, scales = "free")
+
+ggplot(Leptodora, aes(x=Month, y=Density, group=Area)) +
+  geom_line(aes(color=Area))+
+  geom_point(aes(color=Area))+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"),
+        axis.title.x=element_text(size=10),axis.title.y=element_text(size=10),
+        axis.text.x=element_text(size=10),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=14),legend.text = element_text(size=14))+
+  facet_grid(.~Area, scales = "free")
